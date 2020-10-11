5d561e500f7809dfd0b4e91cae1d095b98a88e33
Jiří Vrba <jiri@vrba.dev>
2020-10-11 15:13:53 +0200

    Added linking script

---
+++ link.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+# Link the scripts folder
+ln -s $PWD/.bspwm ~/.bspwm
+
+# Link configurations to the appropriate places
+ln -s $PWD/bspwm ~/.config/bspwm
+ln -s $PWD/sxhkd ~/.config/sxhkd
