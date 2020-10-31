63112a6e9309b491f58daa1ff7d7dcff0c036287
Jiří Vrba <jiri@vrba.dev>
2020-10-31 19:02:43 +0100

    Added missing dependencies

--- check-dependencies.sh
+++ check-dependencies.sh
@@ -9,7 +9,7 @@ function check_command {
 	fi
 }
 
-declare -a requirements=("bspwm" "bspc" "git" "xsecurelock" "unclutter" "feh" "notify-send" "kitty" "rofi" "flameshot" "light" "arandr")
+declare -a requirements=("bspwm" "bspc" "sxhkd" "git" "xsecurelock" "unclutter" "feh" "notify-send" "kitty" "rofi" "flameshot" "light" "arandr" "polybar")
 
 for requirement in ${requirements[@]}; do
 	check_command $requirement
