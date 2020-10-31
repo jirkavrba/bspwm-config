c4b6a91a3955297fa74bfc9f9d11bed638e15862
Jiří Vrba <jiri@vrba.dev>
2020-11-01 00:13:31 +0100

    Added keyboard layouts

--- check-dependencies.sh
+++ check-dependencies.sh
@@ -9,7 +9,7 @@ function check_command {
 	fi
 }
 
-declare -a requirements=("bspwm" "bspc" "sxhkd" "git" "xsecurelock" "unclutter" "feh" "notify-send" "kitty" "rofi" "flameshot" "light" "arandr" "polybar")
+declare -a requirements=("bspwm" "bspc" "sxhkd" "git" "xsecurelock" "unclutter" "feh" "notify-send" "kitty" "rofi" "flameshot" "light" "arandr" "polybar" "dunst")
 
 for requirement in ${requirements[@]}; do
 	check_command $requirement
