9099580b08b2d932f25d20a1d6325fde88fcf19c
Jiří Vrba <jiri@vrba.dev>
2020-11-01 12:22:26 +0100

    Better check for dependencies, added nm-applet as a dependency

--- check-dependencies.sh
+++ check-dependencies.sh
@@ -1,18 +1,25 @@
 #! /bin/env bash
 
+ok=1
+
 function check_command {
 	if ! command -v $1 &> /dev/null
 	then
 	    echo -e "\e[91m$1\e[39m could not be found."
+	    ok=0
 	else
-	    echo -e "\e[32m$1\e[39m OK"
+	    echo "$1 ... OK"
 	fi
 }
 
-declare -a requirements=("bspwm" "bspc" "sxhkd" "git" "xsecurelock" "unclutter" "feh" "notify-send" "kitty" "rofi" "flameshot" "light" "arandr" "polybar" "dunst" "comtpon")
+declare -a requirements=("bspwm" "bspc" "sxhkd" "git" "xsecurelock" "unclutter" "feh" "notify-send" "kitty" "rofi" "flameshot" "light" "arandr" "polybar" "dunst" "compton" "nm-applet")
 
 for requirement in ${requirements[@]}; do
 	check_command $requirement
 done
 
-echo -e "\e[32mAll dependencies ok\e[39m"
+if [ $ok -eq 1 ]; then
+	echo -e "\e[32mAll dependencies ok\e[39m"
+else
+	echo -e "\e[91mOne or more dependencies are missing!\e[39m"
+fi
